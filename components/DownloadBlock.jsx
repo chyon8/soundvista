26c25c3c5bcf76c84a5f6fd235cdb8a2b48480c2
chyon8 <chyon8@hotmail.com>
2024-05-12 18:07:20 +0900

    fix

--- components/DownloadBlock.jsx
+++ components/DownloadBlock.jsx
@@ -2,63 +2,16 @@
 
 import { useState,useEffect } from "react";
 import { faDownload,faCheck } from "@fortawesome/free-solid-svg-icons";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import { useSession } from "next-auth/react";
-
+import { useRouter } from "next/navigation";
 
 const DownloadBlock = ({ id,audioFileTitle,userData }) => {
 
   const {  data: session, status:sessionStatus } = useSession();
-
-
   const [isDownload, setIsDownload] = useState(false);
   const [isSubscribed, setIsSubscribed] = useState(false);
-
-/*
-  useEffect(() => {
-
-    if (sessionStatus !== "authenticated") {
-      return;
-    }
-
-    const fetchUserData = async () => {
-      try {
-        const response = await fetch(`/api/getUserFavorites`, {
-        
-        });
-
-        if (!response.ok) {
-          // Handle error
-          console.error("Failed to fetch user data");
-          return;
-        }
-
-        const userData = await response.json();
-    
-        const downloadsArray = userData.user.downloads || [];
-
-        const userSubStatus=userData.user.subscribed
-        
-
-        // Update the UI based on whether the music id is in the favorites array
-        setIsDownload(Object.keys(downloadsArray).includes(id));
-        setIsSubscribed(userSubStatus)
-        
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    };
-
-    // Fetch user data when the component mounts or when the session changes
-    if (session?.user?.email) {
-      fetchUserData();
-    }
-  }, [id, session]);
-
-*/
-
+const router = useRouter()
 
 useEffect(() => {
 
@@ -81,8 +34,9 @@ useEffect(() => {
 
 
     if (!isSubscribed) {
-        // User is not subscribed, handle accordingly (e.g., show a message)
+
         console.log("User is not subscribed. Download is not allowed.");
+        router.push('/access')
         return;
       }
 
